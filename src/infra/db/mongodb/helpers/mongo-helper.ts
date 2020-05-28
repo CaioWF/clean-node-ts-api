b98b0077a7d551cadfd46b14665bf383677df709
caiowf <caioweliton@alu.ufc.br>
2020-05-28 14:33:45 -0300

    feat: ensure AccountMongoRepository returns an account with success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-import { MongoClient } from 'mongodb';
+import { MongoClient, Collection } from 'mongodb';
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -13,5 +13,9 @@ export const MongoHelper = {
 
   async disconnect() {
     await this.client.close();
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name);
   },
 };
