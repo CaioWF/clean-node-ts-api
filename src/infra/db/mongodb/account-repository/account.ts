c8efe62562ab2ec95bb87202fd33794214d8714b
caiowf <caioweliton@alu.ufc.br>
2020-05-30 12:37:03 -0300

    feat: ensure mongodb reconnect when client is offline

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,11 +1,11 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { AccountModel } from '../../../../domain/models/account';
-import { MongoHelper } from '../helpers/mongo-helper';
+import MongoHelper from '../helpers/mongo-helper';
 
 export default class AccountMongoRepository implements AddAccountRepository {
   async add(account: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts');
+    const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(account);
     return MongoHelper.map(result.ops[0]);
   }
