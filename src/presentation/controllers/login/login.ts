3b700a9f726700581286e8c6513f0b56a3a3375a
caiowf <caioweliton@alu.ufc.br>
2020-06-16 21:14:09 -0300

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { badRequest } from '../../helpers/http-helper';
 import { MissingParamError } from '../../errors';
+import { EmailValidator } from '../signup/signup-protocols';
 
 export default class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
@@ -15,6 +22,9 @@ export default class LoginController implements Controller {
         resolve(badRequest(new MissingParamError('password'))),
       );
     }
+
+    this.emailValidator.isValid(httpRequest.body.email);
+
     return {
       statusCode: 200,
       body: {},
