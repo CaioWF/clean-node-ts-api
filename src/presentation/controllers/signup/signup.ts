cfd9d10d23deae29b035dfae3e0b6c4050966b74
caiowf <caioweliton@alu.ufc.br>
2020-06-17 21:00:31 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -7,19 +7,28 @@ import {
   EmailValidator,
   AddAccount,
 } from './signup-protocols';
+import { Validation } from '../../helpers/validators/validation';
 
 export default class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
 
   private readonly addAccount: AddAccount;
 
-  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
+  private readonly validation: Validation;
+
+  constructor(
+    emailValidator: EmailValidator,
+    addAccount: AddAccount,
+    validation: Validation,
+  ) {
     this.emailValidator = emailValidator;
     this.addAccount = addAccount;
+    this.validation = validation;
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body);
       const requiredFields = [
         'name',
         'email',
