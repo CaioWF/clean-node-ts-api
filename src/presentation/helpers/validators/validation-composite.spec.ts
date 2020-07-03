b7ff103e24a663733211963d467ecb7bb295d561
caiowf <caioweliton@alu.ufc.br>
2020-07-03 12:12:01 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,17 +2,33 @@ import ValidationComposite from './validation-composite';
 import { MissingParamError } from '../../errors';
 import { Validation } from './validation';
 
+interface SutTypes {
+  sut: ValidationComposite;
+  validationStub: Validation;
+}
+
+const makeValidation = (): Validation => {
+  class ValisationStub implements Validation {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    validate(input: any): Error {
+      return null;
+    }
+  }
+  return new ValisationStub();
+};
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const sut = new ValidationComposite([validationStub]);
+  return { sut, validationStub };
+};
+
 describe('Validation Composite', () => {
   test('Should return an error when any validation fails', () => {
-    class ValisationStub implements Validation {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      validate(input: any): Error {
-        return new MissingParamError('field');
-      }
-    }
-
-    const validationStub = new ValisationStub();
-    const sut = new ValidationComposite([validationStub]);
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'));
     const error = sut.validate({ field: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
   });
