c7bc548478ebc3e4e5fb4ba61102d4dbb31a4411
caiowf <caioweliton@alu.ufc.br>
2020-07-24 10:15:32 -0300

    refactor: group validation imports in a single file

---
+++ src/presentation/helpers/validators/index.ts
@@ -0,0 +1,4 @@
+export { default as CompareFieldsValidation } from './compare-fields-validation';
+export { default as EmailValidation } from './email-validation';
+export { default as RequiredFieldValidation } from './required-field-validation';
+export { default as ValidationComposite } from './validation-composite';
