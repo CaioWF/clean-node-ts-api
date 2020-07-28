d8bf027e4d479f406dc717ac72aa400e696a8760
caiowf <caioweliton@alu.ufc.br>
2020-07-28 12:08:07 -0300

    feat: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -28,8 +28,14 @@ export default class DbAuthentication implements Authentication {
       authentication.email,
     );
     if (account) {
-      await this.hashCompare.compare(authentication.password, account.password);
-      await this.tokenGenerator.generate(account.id);
+      const isValid = await this.hashCompare.compare(
+        authentication.password,
+        account.password,
+      );
+      if (isValid) {
+        const accessToken = await this.tokenGenerator.generate(account.id);
+        return accessToken;
+      }
     }
     return null;
   }
