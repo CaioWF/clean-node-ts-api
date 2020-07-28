05c8c2ecf7209554a0080d6a988dc225695c0a10
caiowf <caioweliton@alu.ufc.br>
2020-07-28 11:59:53 -0300

    feat: ensure DbAuthentication calls TokeGenerator with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -4,18 +4,23 @@ import {
 } from '../../../domain/usecases/authentication';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 import { HashCompare } from '../../protocols/criptography/hash-compare';
+import { TokenGenerator } from '../../protocols/criptography/token-generator';
 
 export default class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
 
   private readonly hashCompare: HashCompare;
 
+  private readonly tokenGenerator: TokenGenerator;
+
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashCompare: HashCompare,
+    tokenGenerator: TokenGenerator,
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.hashCompare = hashCompare;
+    this.tokenGenerator = tokenGenerator;
   }
 
   async auth(authentication: AuthenticationModel): Promise<string> {
@@ -24,6 +29,7 @@ export default class DbAuthentication implements Authentication {
     );
     if (account) {
       await this.hashCompare.compare(authentication.password, account.password);
+      await this.tokenGenerator.generate(account.id);
     }
     return null;
   }
