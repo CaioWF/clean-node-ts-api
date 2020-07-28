f62af7530510e0cb3c039110a679c0262f9fe4de
caiowf <caioweliton@alu.ufc.br>
2020-07-28 12:08:59 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -145,4 +145,10 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication());
     await expect(promise).rejects.toThrow();
   });
+
+  test('Should authenticate', async () => {
+    const { sut } = makeSut();
+    const accessToken = await sut.auth(makeFakeAuthentication());
+    expect(accessToken).toBe('any_token');
+  });
 });
